49c506018c061c297ff42d158b1cbe8365494732
Nilolay <98472099+Razaras63@users.noreply.github.com>
2023-02-12 19:41:18 +0400

    Test_1

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbhost = 'localhost';
+$dbusername =    /* 'm92770xs_db'; */  'root'; 
+$dbpassword =   /* 'Niper163!';    */ '';
+$dbname = 'gruz';
+
+$connect = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname);
+
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="/favicon.png">
+    <title>Грузик</title>
+</head>
+<body>
+    <br>тест гит<br>
+    <form action="config.php" method="post">
+        <input type="text" name="login" placeholder="Введите логин"><br><br>
+        <input type="password" name="pass"  placeholder="Введите пароль"><br><br>
+        <input type="submit" value="Вход"><br>
+    </form>
+</body>
+</html>
